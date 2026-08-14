e03cd433715aebeccae6fa9a3d4eccf805c1e3ed
MTPALI <alimtp@atomicmail.io>
2026-08-14 13:10:40 +0330

    Bump experimental hidden receiver build to v0.7.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mtpali.notification"
         minSdk = 28
         targetSdk = 29
-        versionCode = 9
-        versionName = "0.7.2"
+        versionCode = 10
+        versionName = "0.7.3"
     }
 
     buildTypes {
