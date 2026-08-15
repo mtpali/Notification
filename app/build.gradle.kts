06509aa52dc32dd6ab700931f1bf4baf7e711a98
MTPALI <alimtp@atomicmail.io>
2026-08-15 08:29:59 +0330

    Remove unused OkHttp dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,6 @@ android {
 }
 
 dependencies {
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation(platform("com.google.firebase:firebase-bom:34.16.0"))
     implementation("com.google.firebase:firebase-messaging")
 }
